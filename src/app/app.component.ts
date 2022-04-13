986ad23a8bacb007e17f06986ff9a54c19a61da5
Maria <ritalopesmaria94@gmail.com>
2022-04-12 22:46:19 -0300

    site finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+interface Cliente{
+  id: number;
+  servico: number;
+  cliente: string;
+  contato: string;
+}
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'barbeariaPrj';
+ url = "http://lucasreno.kinghost.net/barbearia";
+ fila:Cliente[] = [];
+
+
+ constructor(private http:HttpClient){
+
+  this.pegarDados();
+
+ }
+ 
+pegarDados(){
+  this.http.get<Cliente[]>(this.url).subscribe(
+    resposta => {
+      this.fila = resposta;
+    }
+  );
 }
+
+
+
+}
+
